394b4e86e602b5f9bd2db7cddc9c6e4d1c56ccbb
Hugeface <756148873@qq.com>
2018-10-28 03:34:25 +0800

    统计产品回购率

--- src/main/scala/Lesson_5.scala
+++ src/main/scala/Lesson_5.scala
@@ -15,4 +15,14 @@ object Lesson_5 {
     val rebuyCnt = df.select("product_id", "reordered").where("reordered > 0" ).groupBy("product_id").agg("reordered" -> "sum").toDF("product_id", "reorder_cnt").limit(10)
     rebuyCnt.show()
   }
+  // 统计product再次购买的比例
+  def reorderRatio(): Unit = {
+    val spark = SparkSession.builder().appName("Calculate Product Re-buy Ratio").master("yarn-cluster").getOrCreate()
+    val df = spark.sql("select * from hive.order_products_prior")
+    val prdBuyCnt = df.select("order_id", "product_id").groupBy("product_id").count()
+    val rebuyCnt = df.select("product_id", "reordered").where("reordered > 0" ).groupBy("product_id").agg("reordered" -> "sum").toDF("product_id", "reorder_cnt")
+    val joinTable = prdBuyCnt.join(rebuyCnt, "product_id")
+    val reorderRatio = joinTable.map(x => (x(0).toString, (x(2).toString.toDouble / x(1).toString.toDouble).formatted("%.2f"))).toDF("product_id", "reorder_ratio").limit(10)
+    reorderRatio.show()
+  }
 }
