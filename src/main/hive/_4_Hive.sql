60fd9f0e5f5914e29cc30f3c4a2c2bb47ee25b68
Arete <756148873@qq.com>
2018-10-30 17:30:49 +0800

    Hive查询用户最喜爱的top 10%

--- src/main/hive/_4_Hive.sql
+++ src/main/hive/_4_Hive.sql
@@ -1,11 +1,13 @@
+-- Two tables: order_products_prior, orders
+
 -- 查询每个用户最喜爱的top 10%
-select user_id, ceil((count(1) as double)*0.1) as total_prod_id
+select user_id, ceil(cast(count(distinct pri.product_id) as double)*0.1) as total_prod_cnt
 from orders join order_products_prior pri
 on orders.order_id=pri.order_id
-group by user_id
+group by user_id  -- 根据user_id进行聚合
 limit 10;
 
---
+
 select user_id, product_id,
 row_number() over(partition by user_id order by usr_prod_cnt desc) as row_num
 from
