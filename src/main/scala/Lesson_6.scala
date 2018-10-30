90264619f5b66de33cfec6c2d470fba3d01f9ee6
Arete <756148873@qq.com>
2018-10-30 15:48:03 +0800

    Spark练习题目

---
+++ src/main/scala/Lesson_6.scala
@@ -0,0 +1,9 @@
+object Lesson_6 {
+  /**
+    * user and product Feature: cross feature 交叉特征
+    * 1. 统计user和对应product在多少个订单中出现（distinct order_id）
+    * 2. 特定product具体在购物车中的出现位置的平均位置
+    * 3. 最后一个订单id
+    * 4. 用户对应product在所有这个用户购买产品量中的占比rate
+    * */
+}
