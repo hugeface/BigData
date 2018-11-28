b66357384c512b36dafe20c85d802c565fc6d90f
Arete <756148873@qq.com>
2018-11-28 15:59:17 +0800

    Implement word count based streaming

--- src/main/streaming/SimpleTest.scala
+++ src/main/streaming/SimpleTest.scala
@@ -2,7 +2,7 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 
-object wc {
+object SimpleTest {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(3))
---
+++ src/main/streaming/WordCount.scala
@@ -0,0 +1,31 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(2))
+    // Using updateStateByKey requires the checkpoint directory to be configured
+    ssc.checkpoint("/Users/arete/Source/DATA")
+
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+
+    val addFunc = (curValues:Seq[Long], preValueState:Option[Long]) => {
+      val curCount = curValues.sum
+      val preCount = preValueState.getOrElse(0L)
+      Some(curCount + preCount)
+    }
+
+    val lines = ssc.socketTextStream("localhost", 9999)
+    val words = lines.flatMap(_.split(" "))
+    val wordCount = words.map((_, 1L)).updateStateByKey[Long](addFunc)
+    wordCount.print()
+
+    // Start receiving data and processing it
+    ssc.start()
+    // Wait for the  processing stopped
+    ssc.awaitTermination()
+  }
+}
