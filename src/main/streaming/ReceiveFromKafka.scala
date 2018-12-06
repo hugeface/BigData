0c2987b254974ebe49065e3e30455f3604b28d8c
Hugeface <756148873@qq.com>
2018-12-06 21:53:43 +0800

    Backup

--- src/main/streaming/ReceiveFromKafka.scala
+++ src/main/streaming/ReceiveFromKafka.scala
@@ -18,8 +18,8 @@ object ReceiveFromKafka {
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
 
     // 1.获取参数/指定6个参数
-    val Array(group_id, topic, exectime, dt) = Array("group_test", "stream", "30", "20181206")
-    val zkHostIP = Array("10", "11", "12").map("192.168.136" + _)
+    val Array(group_id, topic, exectime, dt) = Array("brokers", "badou", "30", "20181206")
+    val zkHostIP = Array("10", "11", "12").map("192.168.136." + _)
     val ZK_QUORUM = zkHostIP.map(_+":2181").mkString(",")
     val numPartitions = 1
 
